4e7bf9f8d73e7c7e36847bc880f50bbaab746296
Brianna Fisher <Bnfisher4@gmail.com>
2020-04-20 11:57:02 -0500

    Updates

---
+++ precache-manifest.1d2883e866e29ea221e6b6bdea516c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a9a053b72c25992065296eb0c02547",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "da4320e58ab755464bbe",
+    "url": "/project-4-assessment/static/css/main.c0835aaf.chunk.css"
+  },
+  {
+    "revision": "bf9faed5984ec60f7781",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da4320e58ab755464bbe",
+    "url": "/project-4-assessment/static/js/main.6d5bce4f.chunk.js"
+  },
+  {
+    "revision": "bb8b828653ad1446ab40",
+    "url": "/project-4-assessment/static/js/runtime-main.58f97c80.js"
+  }
+]);
